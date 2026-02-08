0d74cab3b8a478cc711195cfc77d7145f3890175
SK Maqshood <skmaqshood99@gmail.com>
2026-02-08 19:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// write js code here if required
+function showTime() {
+	let now = new Date();
+	let day = now.getDate();
+	let month = now.getMonth()+1;
+	let year = now.getFullYear();
+
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
+	let seconds = now.getSeconds();
+
+	let timeString = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+
+	document.getElementById("timer").textContent= timeString;
+}
+
+setInterval(showTime,1000);
+showTime();
